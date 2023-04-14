1ea83558a47290a99b00a116f15952f490d7d02c
RTDeluna <ramueldeluna30@gmail.com>
2023-04-14 13:12:34 +0800

    final

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import {createGlobalStyle} from "styled-components";
-import {CartContextProvider} from "@/components/CartContext";
+import { CartContextProvider } from '@/components/CartContext';
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
@@ -12,12 +12,12 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <GlobalStyles />
-      <CartContextProvider>
-        <Component {...pageProps} />
-      </CartContextProvider>
-    </>
-  );
+	return (
+		<>
+			<GlobalStyles />
+			<CartContextProvider>
+				<Component {...pageProps} />
+			</CartContextProvider>
+		</>
+	);
 }
